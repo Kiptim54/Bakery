22491cbc49015cdec23b4e3bf5fc1ba030439ffe
Kiptim <kiptim54@gmail.com>
2018-07-15 19:24:48 +0300

    added links and navbar

--- bakeryapp/views.py
+++ bakeryapp/views.py
@@ -8,3 +8,10 @@ def index(request):
     '''
     title="Bakery | Home Page"
     return render(request, 'home/index.html',{"title":title})
+
+def contact(request):
+    '''
+    contact page to receive email from clients
+    '''
+    title='Bakery | Contact us'
+    return render(request, 'home/contact.html', {"title":title})
