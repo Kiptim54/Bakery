ca2958cf3feccbf7e68420fb4def2fcae2209f98
Kiptim <kiptim54@gmail.com>
2018-07-16 18:15:38 +0300

    add linked to db

--- bakery/settings.py
+++ bakery/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'bakery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'bakery',
+        'OWNER':'kiptim',
+        'PASSWORD':'jerotich',
     }
 }
 STATICFILES_FINDERS = [
